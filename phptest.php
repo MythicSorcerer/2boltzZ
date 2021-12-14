b8e8c10b26eb430234dfc2270c159287e35965a9
2boltz <87492725+2boltzZ@users.noreply.github.com>
2021-12-14 14:03:28 -0800

    Update phptest.php

--- phptest.php
+++ phptest.php
@@ -7,7 +7,6 @@
   <title>PHP</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="2boltz" content="">
-  <link rel="stylesheet" href="css/default.css">
 </head>
 
 <body>
